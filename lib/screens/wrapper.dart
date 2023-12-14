11201e8b23b9a3064487c5de556150caf4507ea8
code-plet <70930547+code-plet@users.noreply.github.com>
2023-12-14 17:06:53 +0700

    connect Firebase Files, Plugin FirebaseAuth

---
+++ lib/screens/wrapper.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:socialnetworkapp/screens/auth/auth.dart';
+import 'package:socialnetworkapp/screens/home/home.dart';
+
+class Wrapper extends StatelessWidget {
+  const Wrapper({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Auth();
+  }
+}
